398c9a8bcb4500f0511a18017dc09e5b27efa5c1
Marco A. Peyrot <mpeyrotc@Marcos-MacBook-Pro.local>
2017-09-26 14:41:06 -0500

    Watson Thread working

--- PrimerosAuxilios/app/src/main/java/edu/itesm/mpeyrotc/primerosauxilios/MainActivity.java
+++ PrimerosAuxilios/app/src/main/java/edu/itesm/mpeyrotc/primerosauxilios/MainActivity.java
@@ -1,24 +1,67 @@
 package edu.itesm.mpeyrotc.primerosauxilios;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolBar;
+    TextView textView;
+    EditText editText;
+    Button button;
+
+
+    private class WatsonTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText("running the watson thread.");
+                }
+            });
+
+            return "text to speech done.";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText("TTS Status: " + result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText) findViewById(R.id.edit_field);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
         mToolBar = (Toolbar) findViewById(R.id.nav_action);
         setSupportActionBar(mToolBar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("The text to speech: " + editText.getText());
+                textView.setText("TTS: " + editText.getText());
+
+                WatsonTask task = new WatsonTask();
+                task.execute(new String[]{});
+            }
+        });
     }
 
     @Override
